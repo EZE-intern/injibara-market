49d42cb9c713e1de8ad03d6ce54a2a3620f6a0ea
zed12-pro <zelalemassefa726@gmail.com>
2026-08-17 03:45:30 -0700

    Delete server/routes directory

--- server/routes/cartRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { addItemToCart, getUserCart } = require('../controllers/cartController');
-const { protect } = require('../middleware/authMiddleware'); // { protect } አድርገው
-
-router.post('/', protect, addItemToCart);
-router.get('/', protect, getUserCart);
-
-module.exports = router;
--- server/routes/webhookRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { handleChapaWebhook } = require('../controllers/webhookController');
-
-router.post('/chapa', handleChapaWebhook);
-
-module.exports = router;
