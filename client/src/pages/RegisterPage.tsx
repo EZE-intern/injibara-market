64eea843789fefd4eeb3ad14a1f0281eae758d2f
eyob <eyoelkefale@gmail.com>
2026-08-18 10:26:53 +0300

    adding registration page

---
+++ client/src/pages/RegisterPage.tsx
@@ -0,0 +1,13 @@
+import { FormEvent, useState } from "react";
+import { Link } from "react-router-dom";
+import React from 'react'
+
+function RegisterPage() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default RegisterPage
